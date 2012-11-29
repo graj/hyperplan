878242ba5e26b2aa8f6e0c6f7757588fa605157f
wuhaotian <thelongestmailaccounteverexist@gmail.com>
2012-11-29 20:14:49 +0800

    .

---
+++ hyperplan/HPBubbleGroupViewController.h
@@ -0,0 +1,13 @@
+//
+//  HPBubbleGroupViewController.h
+//  hyperplan
+//
+//  Created by 吴 昊天 on 11/29/12.
+//  Copyright (c) 2012 Sohu Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HPBubbleGroupViewController : UIViewController
+
+@end
---
+++ hyperplan/HPBubbleGroupViewController.m
@@ -0,0 +1,38 @@
+//
+//  HPBubbleGroupViewController.m
+//  hyperplan
+//
+//  Created by 吴 昊天 on 11/29/12.
+//  Copyright (c) 2012 Sohu Inc. All rights reserved.
+//
+
+#import "HPBubbleGroupViewController.h"
+
+@interface HPBubbleGroupViewController ()
+
+@end
+
+@implementation HPBubbleGroupViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
